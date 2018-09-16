bec1909f03e0219ac50285a7832c27ca7e0db7fd
jdp26 <jdp@petticrew.co.uk>
2018-09-16 17:38:41 +0100

    Typo

--- role.distribute.js
+++ role.distribute.js
@@ -1,7 +1,7 @@
 var roleDistribute = {
     /** @param {Creep} creep **/
     run: function(creep) {
-		if(creep.ticksticksToLive<30){
+		if(creep.ticksToLive<30){
 			if(_.sum(creep.carry)>0){
 				console.log('Distributor near death');
 				creep.truck();
