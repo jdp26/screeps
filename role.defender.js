d2f58545f6bc53bf6fe194226b60c54ab820213d
jdp26 <jdp@petticrew.co.uk>
2018-09-16 16:06:56 +0100

    Fix #4, If defenders on room boundary and no hostile creeps, they move off boundary

--- role.defender.js
+++ role.defender.js
@@ -23,6 +23,11 @@ var roleDefender ={
                     }}
                     
                 }
+				else{
+					if(creep.pos.x== 0 || creep.pos.x == 49 || creep.pos.y == 0 || creep.pos.y == 49){
+						creep.moveTo(new RoomPosition(25,25, creep.memory.mine));
+					}
+				}
         }
     },
 	spawn: function(roomname,mineRoom){
