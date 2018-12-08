d54e5f80cc28dc5300280bae45e0eddcc6cc07f1
jdp26 <43293075+jdp26@users.noreply.github.com>
2018-12-08 13:01:04 +0000

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -83,7 +83,7 @@ var roleHarvester = {
         if(harvesters<(num*creepsPerSource)){
 	         
                     if(spawn.spawnCreep(Body, newName, {memory: {role: 'harvester'}})==0){
-			    room.memory.harvesters=room.memory.harvesters+1
+			    room.memory.harvesters=room.memory.harvesters+1;
                     var creep =Game.creeps[newName];
 					creep.memory.home = room.name;
                     var sources = [];
